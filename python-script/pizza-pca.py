05704b928a4c3c9761a949c45fc3b446df0db0fe
Ryan Sloan <ryan@ryanjsloan.com>
2017-07-23 17:14:49 -0700

    added comments

--- python-script/pizza-pca.py
+++ python-script/pizza-pca.py
@@ -1,15 +1,17 @@
 import pandas as pd
 from sklearn.decomposition import PCA
 
-acts = ["Cheese","Pepperoni","Tofu","Jalapenos"]
-legs = ["Washington","Adams","Jefferson","Madison","Monroe"]
-
-
+#read in legislator and vote matrix
 legs_votes = pd.DataFrame.from_csv("pizzaville_votes.csv")
 
 pca = PCA(n_components=1)
 model = pca.fit(legs_votes)
+
+#The component eigenvectors are in model.components_. 
+#Take the first and dot it with each legislator's votes
 scores = legs_votes.dot(model.components_[0])
+
+
 print("Weights:")
 print(model.components_[0])
 
